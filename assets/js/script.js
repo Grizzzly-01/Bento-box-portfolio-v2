d0457bdb23da90d60a2433669872c1710e1926f1
Grizzzly <aatrox543@gmail.com>
2024-07-16 11:21:35 -0700

    susah amat bikin responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,13 @@ document.getElementById('about').addEventListener('click', function(e) {
     e.preventDefault();
     document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
   });
+
+  function project1() {
+    window.open("https://github.com/Grizzzly-01/Expandable-Gallery", "_blank");
+  }
+  function project2() {
+    window.open("https://github.com/Grizzzly-01/Calculator", "_blank");
+  }
+  function project3() {
+    window.open("https://github.com/Grizzzly-01/Blob-Effect", "_blank");
+  }
